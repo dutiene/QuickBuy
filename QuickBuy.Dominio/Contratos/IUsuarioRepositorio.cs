c40deb1ff81deca97120e8a955e28869e5129b3e
paulotiene <du_tiene@hotmail.com>
2019-10-29 23:52:33 -0400

    * IBaseRepositorio.cs: interface Contratos (repositorio)
    * IPedidoRepositorio.cs:
    * IProdutoRepositorio.cs:
    * IUsuarioRepositorio.cs:

---
+++ QuickBuy.Dominio/Contratos/IUsuarioRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using QuickBuy.Dominio.Entidades;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    public interface IUsuarioRepositorio : IBaseRepositorio<Usuario>
+    {
+
+    }
+}
